f2becd52465b4fcff975b33a50d50c6fe7cc0164
RHOMBI <33736460+RHOMBI@users.noreply.github.com>
2018-12-05 20:19:31 +1300

    fixed index bug
    When the user first logs on it now take them to http://localhost/GitHub/login.php instead of http://localhost/GitHub/login/login.php.

--- HtmlSnippets/headerBar.php
+++ HtmlSnippets/headerBar.php
@@ -2,7 +2,7 @@
   session_start();
 
   if (!isset($_SESSION['access_token'])) {
-    header('Location: ../login/login.php');
+    header('Location: ../login.php');
     exit();
   }
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
