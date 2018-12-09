436a147d06079ef976c68e2be38f2f946fd8804e
Zealuss <ZealussGaming@gmail.com>
2018-12-09 19:57:33 +1300

    Started Creating Download functionality

---
+++ TeacherClassAssignment.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>"Students name's" Selections</title>
+		<link rel="stylesheet" type="text/css" href="Styles/main.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="Styles/nav.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="Styles/TeacherClassAssignment.css" />
+	</head>
+
+  	<body>
+  		<?php
+		include ('HtmlSnippets/headerBar.php');	
+		?>
+
+	    <font face = "Verdana">
+		    <div id="main">
+			    <div id ="mainGrid">
+				    <div id = "head">
+						<img src="Images/HPSSLogo.png" alt="HPSS Logo" style="height: 5em">
+						<div>
+							<h1><font face ="Verdana">"Students name's" Selections</font></h1>
+						</div>
+					</div>
+					<div id ="coverage">
+						<div>
+							<a href="">Module and Spin Selection</a>
+						</div>
+						<br>
+						<div>
+							<a href="">Project Selection</a>
+						</div>
+						<br>
+						<div>
+							<a href="">Floortime Selection</a>
+						</div>
+					</div>
+					<div id="profile">
+						Picture<br>
+						Name<br>
+						Hub students and their classes<br>
+						Class they are teaching<br>
+						Timetable<br>
+
+					</div>
+					<div id="selections">
+
+					</div>
+					<footer id="textBox">
+                        <a href="download.php?file=test1.csv">Click here to download the EXCEL CSV file</a><br>
+					</footer>
+				</div>
+			</div>
+		</font>
+	</body>
+</html>
