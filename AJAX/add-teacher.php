2f721fc4624db3af97d45cc3fad725a1a0e9d7a7
RHOMBI <rhombi.gd@gmail.com>
2019-06-04 18:34:39 -0700

    Admin toolds - Teacher Names and Add Teacher
    Teacher names now extracted from database on admin tools.
    A new file has been created named 'add-teacher.php' to add a teacher
    however this is unfinished

---
+++ AJAX/add-teacher.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once "DataBase/database-connect.php";
+$firstname =
+$lastname =
+$email =
+$privilege =
+$kamarcode =
+
+$newteacher = "INSERT INTO `teachers`(`FIRST_NAME`, `LAST_NAME`, `EMAIL`, `TIMETABLING`, `KAMAR_CODE`) VALUES ($firstname,$lastname,$email,$privilege,$kamarcode)";
+
+
+
+?>
