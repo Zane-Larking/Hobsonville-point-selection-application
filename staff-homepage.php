b14f010136e683e1fe82df1d9501516f43935034
RHOMBI <33736460+RHOMBI@users.noreply.github.com>
2020-05-19 15:05:56 +1200

    Teacher aid doesnt show if you have no students
    ^

--- staff-homepage.php
+++ staff-homepage.php
@@ -122,45 +122,48 @@
 								';
 							}
 							if($_SESSION['privilege'] >= 0){
-								echo'
-								<div class="content">
-									<h3><u>Teacher Aid Privileges</u></h3>';
-									$teacher_query = ("SELECT * FROM teacher_aids WHERE teacher_id = ".$_SESSION['id']);
-									$teacher_result = mysqli_query($dbconnect, $teacher_query);
-									if(mysqli_num_rows($teacher_result) != 0) {
-										echo'<h4><u>Propose pupil selection advice</u></h4>';
-									} else {
-										echo'<h4><u>There are no students found to be associated with you</u></h4>';
-									}
-									echo '<br>';
-									if(mysqli_num_rows($teacher_result) != 0) {
-										echo '<div class = "aid-grid-container">';
-										while($row = $teacher_result->fetch_assoc()) {
-											$student_query = ("SELECT * FROM students WHERE ID = ".$row['STUDENT_ID']);
-											$student_result = mysqli_query($dbconnect, $student_query);
-											echo "
-											<div class = 'students-of-teacher-aid'>
-											";
-											while($student_row = $student_result->fetch_assoc()) {
-												echo"
-												<img src= "; if (isset($row['PICTURE'])) echo $row['PICTURE'];  else echo"'Images/portrait-placeholder.png' height='50rem' style= 'grid-area: image;'>
-													<div class = 'ellipsis' style= 'grid-area: name'>".$student_row['FIRST_NAME']." ".$student_row['LAST_NAME']."</div>
-													<div class = 'ellipsis' style= 'grid-area: year-level'>Year ".$student_row['YEAR_LEVEL']."</div>
-													<div class = 'ellipsis' style= 'grid-area: email'>".$student_row['EMAIL']."</div>
-													<div class = 'ellipsis' style= 'grid-area: options'>
-														<a href = 'propose-pupil-selections.php?student=".str_replace(" ", "-", ($student_row['FIRST_NAME'].' '.$student_row['LAST_NAME']))."'>Add selection recommendation</a>
-													</div>
-												";
-											}
-											echo "
-											</div>";
+								$teacher_query = ("SELECT * FROM teacher_aids WHERE teacher_id = ".$_SESSION['id']);
+								$teacher_result = mysqli_query($dbconnect, $teacher_query);
+								if(!$teacher_result){	
+									echo'
+									<div class="content">
+										<h3><u>Teacher Aid Privileges</u></h3>';
+										
+										if(mysqli_num_rows($teacher_result) != 0) {
+											echo'<h4><u>Propose pupil selection advice</u></h4>';
+										} else {
+											echo'<h4><u>There are no students found to be associated with you</u></h4>';
 										}
-										echo "</div>";
-									}
-								//for ($student_with_teacher_aid = 0; $student_with_teacher_aid < ;$student_with_teacher_aid++){
-								//$_SESSION['id']
-								//}
-								echo "</div>" ;
+										echo '<br>';
+										if(mysqli_num_rows($teacher_result) != 0) {
+											echo '<div class = "aid-grid-container">';
+											while($row = $teacher_result->fetch_assoc()) {
+												$student_query = ("SELECT * FROM students WHERE ID = ".$row['STUDENT_ID']);
+												$student_result = mysqli_query($dbconnect, $student_query);
+												echo "
+												<div class = 'students-of-teacher-aid'>
+												";
+												while($student_row = $student_result->fetch_assoc()) {
+													echo"
+													<img src= "; if (isset($row['PICTURE'])) echo $row['PICTURE'];  else echo"'Images/portrait-placeholder.png' height='50rem' style= 'grid-area: image;'>
+														<div class = 'ellipsis' style= 'grid-area: name'>".$student_row['FIRST_NAME']." ".$student_row['LAST_NAME']."</div>
+														<div class = 'ellipsis' style= 'grid-area: year-level'>Year ".$student_row['YEAR_LEVEL']."</div>
+														<div class = 'ellipsis' style= 'grid-area: email'>".$student_row['EMAIL']."</div>
+														<div class = 'ellipsis' style= 'grid-area: options'>
+															<a href = 'propose-pupil-selections.php?student=".str_replace(" ", "-", ($student_row['FIRST_NAME'].' '.$student_row['LAST_NAME']))."'>Add selection recommendation</a>
+														</div>
+													";
+												}
+												echo "
+												</div>";
+											}
+											echo "</div>";
+										}
+									//for ($student_with_teacher_aid = 0; $student_with_teacher_aid < ;$student_with_teacher_aid++){
+									//$_SESSION['id']
+									//}
+									echo "</div>" ;
+								}
 							}
 							if($_SESSION['privilege'] >= 1){
 								echo'
